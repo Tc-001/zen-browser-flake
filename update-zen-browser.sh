bd6dc4e49e2c4184a6a678f0bbc905428b664a5c
Tc001 <55956895+Tc-001@users.noreply.github.com>
2024-12-24 19:00:38 +0200

    Update update-zen-browser.sh

--- update-zen-browser.sh
+++ update-zen-browser.sh
@@ -11,11 +11,11 @@ sed -i "s/version = \".*\"/version = \"$upstream\"/" ./flake.nix
 
 # Update the hash specific.sha256
 specfic=$(nix-prefetch-url --type sha256 --unpack $baseUrl/zen.linux-specific.tar.bz2)
-sed -i "s/specific.sha256 = \".*\"/specific.sha256 = \"$specfic\"/" ./flake.nix
+sed -i "s/specific.sha256 = \".*\"/.sha256 = \"$specfic\"/" ./flake.nix
 
 # Update the hash generic.sha256
 generic=$(nix-prefetch-url --type sha256 --unpack $baseUrl/zen.linux-generic.tar.bz2)
-sed -i "s/generic.sha256 = \".*\"/generic.sha256 = \"$generic\"/" ./flake.nix
+sed -i "s/generic.sha256 = \".*\"/.sha256 = \"$generic\"/" ./flake.nix
 
 nix flake update
 nix build
